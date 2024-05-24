34463d1d3e16d717ebcf743953033dadfd14f97d
TheLazySol <tbelonog25@gmail.com>
2024-05-23 22:15:41 -0400

    Bump version to 0.0.2

--- programs/single-vault-program/src/lib.rs
+++ programs/single-vault-program/src/lib.rs
@@ -3,13 +3,46 @@ use anchor_lang::prelude::*;
 declare_id!("VPgJKrxm2PNrWcF7mnspgqnRLboaoBdVkLcrPZ9qig8");
 
 #[program]
-pub mod single_vault_program {
+pub mod single_vault_program { // Instructions being executed by the program
     use super::*;
 
     pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
+        
         Ok(())
     }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
+#[derive(Accounts)] // Instructions for the accounts
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub user: AccountInfo<'info>,
+
+    #[account(
+        init,
+        payer = user,
+        space = 1024,
+    )]
+
+    pub vault_account: Account<'info, VaultAccount>,
+    pub system_program: Program<'info, System>, // System Program Initializing the vault_account
+}
+
+#[account] // All accounts used and the Data that those accounts require in order fo the instructions to be executed
+
+pub struct VaultAccount { // Struct for the Vault Account
+    is_active: bool,
+    vault_name: String,
+    token_ticker_symbol: String,
+    token_account: Pubkey,
+    mint_address: Pubkey,
+    reward_token: Pubkey,
+    reward_amount: f64,
+    rewards_remaining: f64,
+    time: i64,
+    total_tokens_vaulted: f64,
+    total_rewards_earned: f64,
+    expected_rewards: f64,
+    desposit_amount: f64,
+    annual_percentage_yield_fixed: f64,
+    annual_percentage_yield_variable: f64,
+}
--- tests/single-vault-program.ts
+++ tests/single-vault-program.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SingleVaultProgram } from "../target/types/single_vault_program";
+import { Keypair } from "@solana/web3.js";
 
 describe("single-vault-program", () => {
   // Configure the client to use the local cluster.
@@ -8,9 +9,18 @@ describe("single-vault-program", () => {
 
   const program = anchor.workspace.SingleVaultProgram as Program<SingleVaultProgram>;
 
+  const vaultAccount = new Keypair();
+
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods
+    .initialize()
+    .signers([vaultAccount]) 
+    .accounts({
+      vaultAccount: vaultAccount.publicKey, // Pass the vault account to the program
+    })
+    .rpc({skipPreflight: true});
+    
     console.log("Your transaction signature", tx);
   });
 });
