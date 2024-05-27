683fd1c1fd92c024a8cbc684b78e4d02adc62a23
TheLazySol <tbelonog25@gmail.com>
2024-05-27 00:30:34 -0400

    IGNORE COMMIT -- WIP v0.0.4

--- programs/single-vault-program/src/lib.rs
+++ programs/single-vault-program/src/lib.rs
@@ -1,17 +1,16 @@
 use anchor_lang::prelude::*;
 
-declare_id!("12i6Bo93TrMuSmk9cMiVT1yt1P2hcE8hkB92EWe6YnS5");
+declare_id!("5z6TMA2KJS99AJtZSn51mGLj394Sq4dfcoh36ZmVKeuk");
 
 #[program]
 // Instructions being executed by the program
 pub mod single_vault_program { 
     use super::*;
 
-    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
-        //let vault_account = &mut ctx.accounts.vault_account;    // Initialize the vault_account
-        //vault_account.bump = ctx.bumps.vault_account;                                       // Store the bump see in the vault_account account.
-        
-        msg!("VaultAccount is Initialized.");    // Returns a message that the VaultAccount is Initialized
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let vault_account = &mut ctx.accounts.vault_account;    // Initialize the vault_account
+        vault_account.bump = ctx.bumps.vault_account;                                       // Store the bump seed in the vault_account account.
+        msg!("VaultAccount is Initialized.");                                               // Returns a message on the Blockchain Logs that the VaultAccount is Initialized
         Ok(()) 
     }
 }
@@ -19,32 +18,30 @@ pub mod single_vault_program {
 // Initialize program instructions
 #[derive(Accounts)] 
 pub struct Initialize<'info> {
-    // The user account is mutable because the vault_account is being created by the user and the user is paying for the space.
-    // The vault_account will interact with the user and modify the user account at a later time, hence its mutability.
     #[account(mut)]
-    pub user: AccountInfo<'info>,
+    pub user: Signer<'info>, 
 
     #[account(
         init,                      // Initialize the vault_account
-        seeds = [b"vaultaccount"], // This is the seed for the vault_account
+        seeds = [b"testVault1"],   // This is the seed for the vault_account
         bump,                      // This is the bump seed for the vault_account (which is a PDA) 
         payer = user,              // The user is the payer for the vault_account
         space = 256,               // 256 bytes of space for the vault_account
     )]
     // Initialize accounts and the system_program (native program)
     pub vault_account: Account<'info, VaultAccount>,
-    pub system_program: Program<'info, System>,     // System Program Initializing the vault_account
+    pub system_program: Program<'info, System>,      // System Program Initializing the vault_account
 }
 
-// Vault Information instructions on how to use the VaultAccount Data.
+// This is a Validation Struct in which allows the vault_account PDA to used again in a different instruction. The bump seed for that account is referrenced by bump = vault_account.bump
 #[derive(Accounts)]
-pub struct VaultInformation<'info> {
+pub struct VaultInformation<'info> { 
     #[account(
         mut,
-        seeds = [b"vaultaccount"],                  // optional seeds for the vault_account i.e. "vaultaccount + Keypair"
-        bump = vault_account.bump,                  // bump seed for PDA stored in the "vault_account" Account
+        seeds = [b"testVault1"],                     // optional seeds for the vault_account i.e. "seeds + Keypair"
+        bump = vault_account.bump,                   // bump seed used for the vault_account PDA. Initially the bump + seed was created in the Initialize instruction struct.
     )]
-    pub vault_account: Account<'info, VaultAccount>,// Let the instruction know which account to interact with.
+    pub vault_account: Account<'info, VaultAccount>, // Let the instruction know which account to interact with.
 }
 
 // All accounts used and the Data that those accounts require in order fo the instructions to be executed.
--- tests/single-vault-program.ts
+++ tests/single-vault-program.ts
@@ -1,28 +1,31 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { SingleVaultProgram } from "../target/types/single_vault_program";
-import { PublicKey } from "@solana/web3.js";
+import { SingleVaultProgram } from "/home/skippcode/solana_projects/vaultifi_organization/single-vault-program/target/types/single_vault_program";
+import  { PublicKey } from "@solana/web3.js";
 
-describe("single-vault-program", () => {
+describe("single-vault-program", async () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = (anchor.AnchorProvider.env());
+
+  anchor.setProvider(provider)
 
   const program = anchor.workspace.SingleVaultProgram as Program<SingleVaultProgram>;
 
-  const [vaultaccountPDA] = PublicKey.findProgramAddressSync( // Find the Program Derived Address (PDA) for the vault account
-    [Buffer.from("vaultaccount")],
+  const [vaultAccountPDA] = PublicKey.findProgramAddressSync( // Find the Program Derived Address (PDA) for the vault account
+    
+  [Buffer.from("testVault1")], // Seed
+
     program.programId
   )
 
-  it("Is initialized!", async () => {
-    try {                                                     // Call the initialize method
-      const tx = await program.methods
-      .initialize()
-      .rpc()
-      console.log("Your transaction signature", tx);
-    } catch (error) {                                         // Catch any errors
-      console.log(error);
-    }
-    
+// "it" is a test case. This test case checks if the program is initialized.
+  it("Is initialized!", async () => { 
+
+    const tx = await program.methods
+    .initialize()
+    //.accounts({}) // Add accounts here
+    .rpc({ skipPreflight: true})
+
+    console.log("Transaction executed: ", tx);
   });
 });
